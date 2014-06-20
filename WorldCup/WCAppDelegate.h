9b7b5be283f7c9ada9a9c89477db2f1f60fd2867
Ryan Nystrom <rnystrom@Ryans-MacBook-Pro.local>
2014-06-20 15:50:35 -0400

    Initial Commit

---
+++ WorldCup/WCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WCAppDelegate.h
+//  WorldCup
+//
+//  Created by Ryan Nystrom on 6/20/14.
+//  Copyright (c) 2014 Ryan Nystrom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WorldCup/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WorldCup
+//
+//  Created by Ryan Nystrom on 6/20/14.
+//  Copyright (c) 2014 Ryan Nystrom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WCAppDelegate class]));
+    }
+}
